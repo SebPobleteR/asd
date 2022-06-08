96e9fe3e9192e780385e63f59473b6df87d3b93b
SebPobleteR <102103241+SebPobleteR@users.noreply.github.com>
2022-06-08 01:05:38 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,31 +4,17 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 def process_message(update, context):
-
-    
-
+    text=update.message.text
     if str(text).__contains__('#channel'):
         context.bot.send_message(
             chat_id='YOUR_CHANNEL_ID',
             text=str(text).replace('#channel', '')
         )
-    else:
-        print('fa')
-    
         
-
-
 if __name__ == '__main__':
-
     updater = Updater(token=os.environ['TOKEN'], use_context=True)
-   
-
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))
     updater.start_polling()
-
     print('Bot is polling')
-    
-    
-
     updater.idle()
