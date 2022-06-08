85e1de7780e6abaf11402e89949032db6122bb38
SebPobleteR <102103241+SebPobleteR@users.noreply.github.com>
2022-06-08 00:44:21 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+import telegram
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+
+def handle_start(update, context):
+
+    update.message.reply_text(
+        text=(
+            'This bot has been migrated to a new one: @ForwarderGeniusBot.'
+            '\nGo there and run /start to continue'
+        ),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Go to the bot', url='https://t.me/ForwarderGeniusBot')]
+        ])
+    )
+
 
 
 def process_message(update, context):
@@ -16,11 +31,14 @@ def process_message(update, context):
 if __name__ == '__main__':
 
     updater = Updater(token=os.environ['TOKEN'], use_context=True)
+    bot = telegram.Bot(token=token)
 
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))
     updater.start_polling()
 
     print('Bot is polling')
+    
+    print(f'running at @{bot.username}')
 
     updater.idle()
